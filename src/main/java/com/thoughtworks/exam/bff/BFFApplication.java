93fac7d4498bb7c01b71dd042148a1c606054aeb
Yuan Shenjian <sjyuan@thoughtworks.com>
2020-06-08 15:55:01 +0800

    Init project
    Signed-off-by: Yuan Shenjian <sjyuan@thoughtworks.com>

---
+++ src/main/java/com/thoughtworks/exam/bff/BFFApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.exam.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BFFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BFFApplication.class, args);
+	}
+
+}
